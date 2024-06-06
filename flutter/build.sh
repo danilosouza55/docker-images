81a19a586827c9dce421f9a0486420ba1a3f5d66
Danilo Araújo de Souza <danilo.asouza@hotmail.com>
2024-06-05 21:15:45 -0300

    add flutter v3.22.1

--- flutter/build.sh
+++ flutter/build.sh
@@ -1,4 +1,9 @@
 #/bin/bash
+docker buildx create --use
+
 cd 3.19.2
 docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:3.19.2 --push .
+
+cd 3.22.1
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:3.22.1 --push .
 docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:latest --push .
