7097191c20ec18edcbc53f42933fdddd4a394a27
Danilo Araújo de Souza <danilo.asouza@hotmail.com>
2024-06-05 21:35:05 -0300

    correção no script de build

--- flutter/build.sh
+++ flutter/build.sh
@@ -4,6 +4,7 @@ docker buildx create --use
 cd 3.19.2
 docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:3.19.2 --push .
 
+cd ..
 cd 3.22.1
 docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:3.22.1 --push .
 docker buildx build --platform linux/amd64,linux/arm64/v8 -t danilo55555/flutter-sdk:latest --push .
